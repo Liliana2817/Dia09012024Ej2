ab6c3086650606fb805776491be5684717b2fce1
Liliana2817 <garciasevericheliliana@gmail.com>
2024-01-09 11:58:01 +0100

    subir

---
+++ Dia09012024Ej2/src/Dia09012024Ej2/Ejercicio2.java
@@ -0,0 +1,23 @@
+package Dia09012024Ej2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	// 2.Simulador de Cajero Automático 
+			//  - Descripción: Desarrollar un simulador de cajero automático que permita retirar dinero, depositar y consultar saldo. El programa debe manejar cuentas múltiples con ID y contraseña.
+			//  - Retos: Crear un sistema de autenticación, manejar el saldo de las cuentas, y realizar operaciones básicas de un cajero automático.
+	        /*2.Simulador de Cajero Automático
+	   -Pasos:
+	     1. Crea estructuras para almacenar la información de las cuentas (por ejemplo, usando arrays para IDs, contraseñas y saldos).
+	     2. Implementa un sistema de login que pida al usuario su ID y contraseña y verifique estos datos.
+	     3. Una vez autenticado, muestra un menú con opciones como retirar, depositar y ver saldo.
+	     4. Para cada opción, realiza las operaciones correspondientes (actualizar saldo, mostrar información, etc.).
+	     5. Asegúrate de validar las entradas del usuario (por ejemplo, no permitir retirar más dinero del disponible en la cuenta).
+	*/
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+
+	}
+
+}
